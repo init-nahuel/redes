4127890de1df519ccbc2028c0f359d098991ccf4
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-03-26 16:15:45 -0300

    doc: documentation functions

--- actividades/request_http/utils.py
+++ actividades/request_http/utils.py
@@ -1,67 +1,88 @@
-# import os
+import os
 import re
 
-# Retorna -1 si el header Content-Length no se encuentra por completo en message, 
-# de otra forma retorna el valor de este header.
-# OBS: la expresion regular como tal no necesitaria hacer math de espacios luego del numero
-# pues por formato debiese de ir de inmediato '\r\n'
-def contains_length_header(message):
-    msg = message.lower()
-    regex = re.compile(r'Content-Length: (\d*)\r\n')
+response = """POST /cgi-bin/process.cgi HTTP/1.1
+User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)
+Host: www.tutorialspoint.com
+Content-Type: text/xml; charset=utf-8
+Content-Length: 123
+Accept-Language: en-us
+Accept-Encoding: gzip, deflate
+Connection: Keep-Alive
+
+# <?xml version="1.0" encoding="utf-8"?>
+# <string xmlns="http://clearforest.com/">string</string>"""
+
+# response = """POST /cgi-bin/process.cgi HTTP/1.1\r\nUser-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\r\nHost: www.tutorialspoint.com\r\nContent-Type: text/xml; charset=utf-8\r\nContent-Length: 123\r\nAccept-Language: en-us\r\nAccept-Encoding: gzip, deflate\r\nConnection: Keep-Alive\r\n\r\n<?xml version="1.0" encoding="utf-8"?><string xmlns="http://clearforest.com/">string</string>"""
+
+def content_length_header(message):
+    """Retorna -1 si el header Content-Length no se encuentra por completo en message,
+    de otra forma retorna el largo declarado.
+    La expresion regular se rige por el formato estandar que debiesen tener los mensajes HTTP.
+    """
+
+    # regex = re.compile(r'Content-Length: (\d+)\r\n')
+    regex = re.compile(r'Content-Length: (\d+)'+os.linesep)
     content_lenght = regex.search(message)
     try:
-        return content_lenght.group(1)
-    except:
+        return int(content_lenght.group(1))
+    except AttributeError:
         return -1
+    
+assert content_length_header(response) == 123
 
-# esta función se encarga de recibir el mensaje completo desde el cliente
-# en caso de que el mensaje sea más grande que el tamaño del buffer 'buff_size', esta función va esperar a que
-# llegue el resto
+def receive_full_mesage_http(connection_socket, buff_size, end_sequence='\r\n\r\n'):
+    """Recibe el mensaje HTTP completo desde el cliente, primero recibe el HEAD
+    y luego recibe el BODY a traves de la obtencion
+    del largo de este, por ultimo retorna el mensaje HTTP decodificado.
+    Por ahora se considera que el contenido de los mensajes es texto plano y no
+    imagenes, por eso se retorna
+    decodificado el mensaje.
+    """
 
-def receive_full_mesage_http(connection_socket, buff_size):
-
-    # recibimos la primera parte del mensaje
-    end_sequence = '\r\n'
     recv_message = connection_socket.recv(buff_size)
     full_message = recv_message
 
-    # verificamos si llegó el mensaje completo o si aún faltan partes del mensaje
-    is_end_of_message = contains_end_of_message(full_message.decode(), end_sequence)
+    is_end_of_message = contains_end_of_head(full_message.decode(), end_sequence)
 
-    # Obtenemos el HEAD
+    # Recibimos el HEAD (\r\n\r\n)
     while not is_end_of_message:
-        # recibimos un nuevo trozo del mensaje
         recv_message = connection_socket.recv(buff_size)
 
-        # lo añadimos al mensaje "completo"
         full_message += recv_message
 
-        # verificamos si es la última parte del mensaje
-        is_end_of_message = contains_end_of_message(full_message.decode(), end_sequence)
+        is_end_of_message = contains_end_of_head(full_message.decode(), end_sequence)
 
-    # removemos la secuencia de fin de mensaje, esto entrega un mensaje en string
-    full_message = remove_end_of_message(full_message.decode(), end_sequence)
+    # Recibimos el BODY
+    full_message = full_message.decode()
+    index_begin_body = full_message.index(end_sequence) + len(end_sequence) - 1
+    content_length = content_length_header(full_message)
+    body_buff_size = content_length - len(full_message[index_begin_body:])
+    print(f"body_buff_size: {body_buff_size}")
+    print(f"content_length: {content_length}")
+    full_message += connection_socket.recv(body_buff_size).decode()
 
-    # finalmente retornamos el mensaje
     return full_message
 
-
-def contains_end_of_message(message, end_sequence):
-    return message.endswith(end_sequence)
-
-
-def remove_end_of_message(full_message, end_sequence):
-    index = full_message.rfind(end_sequence)
-    return full_message[:index]
+def contains_end_of_head(message, end_sequence):
+    return end_sequence in message
 
 def from_http_to_data(message):
+    """Retorna un diccionario con los componentes del mensaje HTTP.
+    Las llaves se encuentran clasificadas en start_line, headers y body.
+    Por ahora considera los separadores del formato estandar de envio de mensajes HTTP.
+    Es necesario mencionar que dada la forma en que se hace el split de los string para separar
+    los headers de la descripcion de este, a la hora de volver a generar el mensaje HTTP
+    con from_data_to_http(), el mensaje generado volvera a mantener los estandares
+    de formato.
+    """
     # Las lineas comentadas sirven en caso de que el mensaje HTTP se encuentre con saltos de linea y no un solo un string con los
     # respectivos \r\n y \r\n\r\n
-    head, body = message.split('\r\n\r\n')
-    # head, body = message.split(2*os.linesep)
+    # head, body = message.split('\r\n\r\n')
+    head, body = message.split(2*os.linesep)
 
-    headers = head.split('\r\n')
-    # headers= head.split('\n')
+    # headers = head.split('\r\n')
+    headers= head.split(os.linesep)
 
     start_line = headers[0]
     http_dict = {'start_line': start_line}
@@ -75,6 +96,8 @@ def from_http_to_data(message):
     return http_dict
 
 def from_data_to_http(http_dict):
+    """Retorna el mensaje HTTP original"""
+
     http_message = http_dict['start_line'] + '\r\n'
     del http_dict['start_line']
 
@@ -86,22 +109,3 @@ def from_data_to_http(http_dict):
         del http_dict[key]
 
     return http_message
-    
-
-
-response = """POST /cgi-bin/process.cgi HTTP/1.1\r\nUser-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\r\nHost: www.tutorialspoint.com\r\nContent-Type: text/xml; charset=utf-8\r\nContent-Length: length\r\nAccept-Language: en-us\r\nAccept-Encoding: gzip, deflate\r\nConnection: Keep-Alive\r\n\r\n<?xml version="1.0" encoding="utf-8"?><string xmlns="http://clearforest.com/">string</string>"""
-
-# response = """POST /cgi-bin/process.cgi HTTP/1.1
-# User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)
-# Host: www.tutorialspoint.com
-# Content-Type: text/xml; charset=utf-8
-# Content-Length: length
-# Accept-Language: en-us
-# Accept-Encoding: gzip, deflate
-# Connection: Keep-Alive
-
-# <?xml version="1.0" encoding="utf-8"?>
-# <string xmlns="http://clearforest.com/">string</string>"""
-
-http_dict = from_http_to_data(response)
-http_message = from_data_to_http(http_dict)
