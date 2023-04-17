28e3e6953688dda1f07161e6b38814b04b771f95
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-04-16 22:26:03 -0400

    add: use resolver workin correctly

--- actividades/resolver_dns/resolver.py
+++ actividades/resolver_dns/resolver.py
@@ -2,16 +2,20 @@ import socket
 from utils import *
 from dnslib import DNSRecord
 
-resolver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+new_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 address = ('localhost', 8000)
-resolver_socket.bind(address)
+new_socket.bind(address)
 
 while True:
-    query_msg, address_query = resolver_socket.recvfrom(4096)
+    query_msg, address_query = new_socket.recvfrom(4096)
     print("<<----------------------------->>")
     print(DNSRecord.parse(query_msg))
     print("<<----------------------------->>")
-    print(parse_dns_msg(query_msg))
-    print("<<----------------------------->>")
-    resolver(query_msg)
+    
+    resolver_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    response = resolver(query_msg, resolver_socket)
+    if response == '':
+        pass # Ke hago?
+    
+    new_socket.sendto(response, address_query)
