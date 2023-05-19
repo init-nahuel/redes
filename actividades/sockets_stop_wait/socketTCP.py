fba037b678540941b15f0ebe40db70031f626b76
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-05-18 22:03:04 -0400

    feat: SocketTCP draft, handshake working :D

--- actividades/sockets_stop_wait/socketTCP.py
+++ actividades/sockets_stop_wait/socketTCP.py
@@ -1,10 +1,9 @@
+from __future__ import annotations
 import socket
 import random
-import os
-import socketTCP
 
 
-class socketTCP:
+class SocketTCP:
     def __init__(self) -> None:
         self.dest_address = None
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -44,11 +43,12 @@ class socketTCP:
 
     @staticmethod
     def verify_seq_3way(new_seq: int, actual_seq: int) -> bool:
-        """Verifica que el numero de secuencia nuevo se haya incrementado en 1
-        durante el 3-way handshake, en tal caso retorna True y False en caso contrario.
+        """Verifica que el numero de secuencia nuevo new_seq se haya incrementado en 2
+        durante los pasos del 3-way handshake, con respecto al numero de secuencia actual_seq, que posee
+        el emisor, en tal caso retorna True y False en caso contrario.
         """
 
-        return new_seq - actual_seq == 1
+        return new_seq - actual_seq == 2
 
     @staticmethod
     def verify_inc_seq(new_seq: int, actual_seq: int) -> tuple[bool, int]:
@@ -60,14 +60,14 @@ class socketTCP:
         return (diff > 0, diff)
 
     def bind(self, address: tuple[str, int]):
-        """Aisgna una direccion de origen (IP, Puerto) al socket socketTCP.
+        """Aisgna una direccion de origen (IP, Puerto) al socket SocketTCP.
         """
 
         self.orig_address = address
         self.udp_socket.bind(address)
 
     def connect(self, address) -> int:
-        """Establece el 3-way handshake entre el socket socketTCP cliente y
+        """Establece el 3-way handshake entre el socket SocketTCP cliente y
         el servidor ubicado en la direcction de destino address
         en la que se encuentra escuchando. Retorna uno en caso de establecerse correctamente
         el handshake y 0 en caso contrario.
@@ -79,88 +79,125 @@ class socketTCP:
         status = 0
 
         # 3-way handshake
+
         # Envio mensaje SYN Cliente
         self.header_buffer = self.make_tcp_headers(1, 0, 0, self.seq)
+
+        print(f"----> Enviando mensaje SYN a Servidor: {self.header_buffer}")
         self.udp_socket.sendto(self.header_buffer.encode(), self.dest_address)
 
         # Recibo SYN+ACK Servidor
-        self.header_buffer, _ = self.udp_socket.recvfrom(16)
-
+        self.header_buffer, new_socket_address = self.udp_socket.recvfrom(16)
         self.header_buffer = self.header_buffer.decode()
+        print(
+            f"----> Recibido mensaje SYN+ACK desde Servidor: {self.header_buffer}")
+
+        self.dest_address = new_socket_address
         self.parsed_buffer = self.parse_segment(self.header_buffer)
-        syn, ack, fin, new_seq, _ = [int(val)
-                                     for val in self.parsed_buffer.values()]
-        if (self.verify_seq_3way(new_seq, self.seq) and syn == 1 and ack == 1 and fin == 0):
-            self.seq = new_seq + 1
-            self.header_buffer = self.make_tcp_headers(0, 1, 0, self.seq)
+        syn, ack, fin, new_seq = [int(val)
+                                  for val in self.parsed_buffer.values()]
+
+        # En verify_seq_3way sumamos 1 pues en el primer SYN del Cliente va el SEQ sin incremento
+        if (self.verify_seq_3way(new_seq + 1, self.seq) and syn == 1 and ack == 1 and fin == 0):
+            self.seq = new_seq
 
             # Envio ACK Cliente
+            self.header_buffer = self.make_tcp_headers(0, 1, 0, self.seq + 1)
+
+            print(
+                f"----> Enviando mensaje ACK de confirmacion a Servidor: {self.header_buffer}")
             self.udp_socket.sendto(
-                self.header_buffer.enconde(), self.dest_address)
+                self.header_buffer.encode(), self.dest_address)
+        else:
+            print("----> Posible infiltracion en la conexion, cerrando...")
+
+            return status
+
+        # Recibo ACK Servidor (Stop & Wait)
+        self.header_buffer, _ = self.udp_socket.recvfrom(16)
+        self.header_buffer = self.header_buffer.decode()
+        print(
+            f"----> Recibido mensaje ACK por Servidor, paso Stop & Wait: {self.header_buffer}")
+
+        self.parsed_buffer = self.parse_segment(self.header_buffer)
+        syn, ack, fin, new_seq = [int(val)
+                                  for val in self.parsed_buffer.values()]
+
+        if (self.verify_seq_3way(new_seq, self.seq) and syn == 0 and ack == 1 and fin == 0):
+            self.seq = new_seq
 
             status = 1
             return status
         else:
-            print("----> Posible infiltracion en la conexion, cerrando...")
-            os.close()
 
             return status
 
-    def accept(self) -> tuple[socketTCP, tuple[str, int]]:
-        """Establece el 3-way handshake entre el socket socketTCP Servidor y el cliente.
-        Retorna una tupla con un nuevo socket socketTCP y la direccion de este nuevo socket."""
+    def accept(self) -> tuple[SocketTCP, tuple[str, int]] | None:
+        """Establece el 3-way handshake entre el socket SocketTCP Servidor y el cliente.
+        Retorna una tupla con un nuevo socket SocketTCP y la direccion de este nuevo socket."""
+
+        # Creamos el nuevo socket para el
+        new_socket = SocketTCP()
 
         # 3-way handshake
         # Recibo SYN Cliente
-        self.header_buffer, client_address = self.udp_socket.recvfrom(16)
-        self.header_buffer = self.header_buffer.decode()
-        self.parsed_buffer = self.parse_segment(self.header_buffer)
-        self.seq = int(self.parsed_buffer['SEQ']) + 1
+        new_socket.header_buffer, client_address = self.udp_socket.recvfrom(16)
+        new_socket.dest_address = client_address
+
+        # Bind del nuevo socket
+        # new_socket.bind(client_address)
+
+        new_socket.header_buffer = new_socket.header_buffer.decode()
+        print(
+            f"----> Recibido mensaje SYN por Cliente: {new_socket.header_buffer}")
+
+        new_socket.parsed_buffer = new_socket.parse_segment(
+            new_socket.header_buffer)
+        new_socket.seq = int(new_socket.parsed_buffer['SEQ'])
         syn, ack, fin, _ = [int(val)
-                            for val in self.header_buffer.split('|||')]
+                            for val in new_socket.header_buffer.split('|||')]
 
         if (syn == 1 and ack == 0 and fin == 0):
-            self.header_buffer = self.make_tcp_headers(1, 1, 0, self.seq)
+            new_socket.header_buffer = new_socket.make_tcp_headers(
+                1, 1, 0, new_socket.seq + 1)
 
             # Envio SYN+ACK
-            self.udp_socket.sendto(self.header_buffer, client_address)
+            print(
+                f"----> Enviando mensaje SYN+ACK a Cliente: {new_socket.header_buffer}")
+            new_socket.udp_socket.sendto(
+                new_socket.header_buffer.encode(), client_address)
 
         else:
-            print("----> Recibido peticion erronea, rechazando...")
-            self.seq = None
-            self.header_buffer = None
-            self.parsed_buffer = None
+            print(
+                "----> Recibido peticion erronea, se esperaba recibir SYN , rechazando...")
 
             return None
 
         # Recibo ACK Cliente
-        self.header_buffer, _ = self.udp_socket.recvfrom(16)
-        self.header_buffer = self.header_buffer.decode()
-        self.parsed_buffer = self.parse_segment(self.header_buffer)
+        new_socket.header_buffer, _ = new_socket.udp_socket.recvfrom(16)
+        new_socket.header_buffer = new_socket.header_buffer.decode()
+        print(
+            f"----> Recibido mensaje ACK por Cliente: {new_socket.header_buffer}")
+
+        new_socket.parsed_buffer = new_socket.parse_segment(
+            new_socket.header_buffer)
         syn, ack, fin, new_seq = [int(val)
-                                  for val in self.header_buffer.split('|||')]
-        if (syn == 1 and ack == 0 and fin == 0 and self.verify_seq_3way(new_seq, self.seq)):
-            self.seq = new_seq + 1
-            # Envio ACK
-            self.udp_socket.sendto(self.make_tcp_headers(
-                0, 1, 0, self.seq), client_address)
+                                  for val in new_socket.header_buffer.split('|||')]
+        if (syn == 0 and ack == 1 and fin == 0 and new_socket.verify_seq_3way(new_seq, new_socket.seq)):
+            new_socket.seq = new_seq
 
-            # Eliminamos direccion y seq del socket Server y trapasamos estos datos al nuevo socketTCP
-            new_address = self.orig_address + random.randint(1, 100)
-            bind_address = self.orig_address
-            self.orig_address = new_address
-            self.seq = 0
+            # Envio ACK (Stop & Wait)
+            new_socket.header_buffer = new_socket.make_tcp_headers(
+                0, 1, 0, new_socket.seq + 1)
+            print(
+                f"----> Enviando mensaje ACK a Cliente, paso Stop & Wait: {new_socket.header_buffer}")
 
-            new_socket = socketTCP()
-            new_socket.seq = self.seq
-            new_socket.bind(bind_address)
-            new_socket.dest_address = client_address
+            new_socket.udp_socket.sendto(
+                new_socket.header_buffer.encode(), client_address)
 
-            # Devolvemos nueva instancia de socketTCP
-            return (new_socket, bind_address)
+            # Devolvemos nueva instancia de SocketTCP
+            return (new_socket, new_socket.orig_address)
         else:
-            print("----> Handshake erroneo, rechanzando...")
-            self.seq = None
-            self.header_buffer = None
-            self.parsed_buffer = None
+            print("----> Handshake erroneo, se esperaba recibir ACK, rechanzando...")
+
             return None
