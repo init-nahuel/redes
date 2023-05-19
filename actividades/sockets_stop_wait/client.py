3ba7065489b8fbcef85c0daf4bf22059c7cbf50c
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-05-18 22:04:47 -0400

    add: drafts for server and client handshake

--- actividades/sockets_stop_wait/client.py
+++ actividades/sockets_stop_wait/client.py
@@ -1,19 +1,15 @@
-import socket
-import sys
-from utils import *
+from socketTCP import SocketTCP
 
-# python3 cliente.py localhost 8000 < archivo.txt
-program_name, ip, port = sys.argv
-file = input()
+# program_name, ip, port = sys.argv
+# file = input()
 
-# OJO CON EL EOF CUANDO EL ARCHIVO ESTA VACIO, LANZA LA EXEPCION EOFError
+address = ('localhost', 8000)
 
-address = (ip, int(port))
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# address = (ip, int(port))
+client_socketTCP = SocketTCP()
 
-print(file)
-# Enviamos los trozos de 16 bytes
-# msg_bytes = file.encode()
-# send_message(msg_bytes, client_socket, address, size=len(msg_bytes))
+print("----> Realizando Handshake")
 
-# client_socket.close()
+client_socketTCP.connect(address)
+
+print("----> Handshake finalizado")
--- actividades/sockets_stop_wait/server.py
+++ actividades/sockets_stop_wait/server.py
@@ -1,9 +1,13 @@
-import socket
+# import socket
+from socketTCP import SocketTCP
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 address = ('localhost', 8000)
-server_socket.bind(address)
 
-while True:
-    msg,_ = server_socket.recvfrom(16)
-    print(msg.decode())
+server_socketTCP = SocketTCP()
+server_socketTCP.bind(address)
+
+print("----> Realizando Handshake")
+
+connection_socketTCP, new_address = server_socketTCP.accept()
+
+print("----> Handshake finalizado")
