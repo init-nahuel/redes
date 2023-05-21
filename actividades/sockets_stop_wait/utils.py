f1ac1104c85f5ace3c4a4ab0ee356b09ad802f62
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-05-20 22:40:09 -0400

    fix: utils methods

--- actividades/sockets_stop_wait/utils.py
+++ actividades/sockets_stop_wait/utils.py
@@ -1,14 +1,36 @@
+from socketTCP import socketTCP
 import socket
 
-def send_message(file: bytes, socket: socket.socket, address: tuple[str, str], size: int) -> None:
-    """Envia el archivo file mediante el socket hacia la direccion address, si el
+
+def send_message(file: bytes, socket: socket.socket, address: tuple[str, int], header_tcp,  size: int) -> None:
+    """Envia el archivo file mediante el socket UDP hacia la direccion address, si el
     mensaje es de largo mayor a 16 bytes lo divide en paquetes de este largo
     maximo y los envia secuencialmente.
     """
-    
+
+    # Caso el largo del mensaje es mayor a 16 bytes
     while (size > 16):
-        socket.sendto(file[:16], address) # Enviamos los primeros 16 bytes
+        socket.sendto(header_tcp.encode() + file[:16], address)
         file = file[16:]
         size = len(file)
-    
-    socket.sendto(file, address)
+
+    # Enviamos el mensaje completo si es menor a 16 bytes o lo
+    # restante en caso de haber pasado por el while
+    socket.sendto(header_tcp.encode() + file, address)
+
+
+def receive_message(rcv_socket: socket.socket):
+
+    # Primero recibimos el header (largo fijo codificado: 16)
+    msg, address = rcv_socket.recvfrom(16)
+
+    # TODO: Mas adelante hay que manejar el SEQ del header para saber el largo del contenido
+
+    if (int(socketTCP.parse_segment(msg.decode())['SYN']) == 1 or int(socketTCP.parse_segment(msg.decode())['ACK']) == 1):
+        return (msg.decode(), "", address)
+
+    content_msg, _ = rcv_socket.recvfrom(16)
+
+    # Retorna el header decodificado y el contenido del mensaje codificado
+    # luego deben unirse todas las partes para decodificarlo
+    return (msg.decode(), content_msg, address)
