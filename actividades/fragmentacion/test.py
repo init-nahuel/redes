862ff85b10cf8264358bd0a7ae5424dcec6cd362
Nahuel <nahuelmath@gmail.com>
2023-06-13 13:33:45 -0400

    add: testing template

--- actividades/fragmentacion/test.py
+++ actividades/fragmentacion/test.py
@@ -13,6 +13,11 @@ def main():
     for p in fragments_list:
         print(p.decode())
         print("-----------------")
+    
+    print(new_router.reassemble_IP_packet(fragments_list))
+
+    fragment_fragment_list = new_router.fragment_IP_packet(fragments_list[1], 36)
+    print(fragment_fragment_list)
 
 
 if __name__ == '__main__':
