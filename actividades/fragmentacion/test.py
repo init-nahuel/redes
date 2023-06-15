98cb61c4182c98460a0cb7ce7964c214b3788ec5
Nahuel <nahuelmath@gmail.com>
2023-06-15 13:11:29 -0400

    add: testing to reassembly packet

--- actividades/fragmentacion/test.py
+++ actividades/fragmentacion/test.py
@@ -8,16 +8,18 @@ def main():
     packet = "127.0.0.1,8885,10,347,0,00000005,0,hola!"
     fragments_list = new_router.fragment_IP_packet(packet.encode(), 38)
 
-    print(fragments_list)
+    print(f"----> Paquete a framentar: {packet}")
 
-    for p in fragments_list:
-        print(p.decode())
-        print("-----------------")
+    print("----> Lista de framentos: ")
+    for f in fragments_list:
+        print(f.decode() + "\n")
+    print("--------------------------------")
     
-    print(new_router.reassemble_IP_packet(fragments_list))
+    ensambled_fragments = new_router.reassemble_IP_packet(fragments_list)
+    print(f"----> Paquete reensamblado: {ensambled_fragments}")
 
-    fragment_fragment_list = new_router.fragment_IP_packet(fragments_list[1], 36)
-    print(fragment_fragment_list)
+    # fragment_fragment_list = new_router.fragment_IP_packet(fragments_list[1], 36)
+    # print(fragment_fragment_list)
 
 
 if __name__ == '__main__':
