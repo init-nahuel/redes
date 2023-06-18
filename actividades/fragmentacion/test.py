09fa8491a738d4231da98a6f5ff9b4127fffeae2
Nahuel Llancapichun <nahuelmath@gmail.com>
2023-06-17 21:57:27 -0400

    add: reensambling fragment test

--- actividades/fragmentacion/test.py
+++ actividades/fragmentacion/test.py
@@ -6,29 +6,28 @@ def main():
     new_router = router.Router()
 
     packet = "127.0.0.1,8885,10,347,0,00000005,0,hola!"
-    fragments_list = new_router.fragment_IP_packet(packet.encode(), 38)
-
     print(f"----> Paquete a framentar: {packet}")
+    fragments_list = new_router.fragment_IP_packet(packet.encode(), 38)
 
     print("----> Lista de framentos: ")
     for f in fragments_list:
-        print(f.decode() + "\n")
+        print("\n" + f.decode())
     print("--------------------------------")
-    
+
+    fragment = fragments_list[0]
+    fragment_fragments_list = new_router.fragment_IP_packet(fragment, 36)
+    print(f"----> Fragmentando primer fragmento: {fragment.decode()}")
+    print("----> Se crearon los siguientes fragmentos:")
+    fragments_list.remove(fragment)
+
+    for f in fragment_fragments_list:
+        print("\n" + f.decode())
+    print("--------------------------------")
+    print("----> Reensamblando")
+
+    fragments_list += fragment_fragments_list
     ensambled_fragments = new_router.reassemble_IP_packet(fragments_list)
     print(f"----> Paquete reensamblado: {ensambled_fragments}")
-
-    # fragment_fragment_list = new_router.fragment_IP_packet(fragments_list[1], 36)
-    # print(fragment_fragment_list)
-
-    print("\n-------------TEST Item 5:-------------")
-    ip_packet_v1 = "127.0.0.1,8885,10,347,0,00000005,0,hola!"
-    MTU = 38
-    fragment_list = new_router.fragment_IP_packet(ip_packet_v1.encode(), MTU)
-    ip_packet_v2_str = new_router.reassemble_IP_packet(fragment_list)
-    ip_packet_v2 = ip_packet_v2_str.encode()
-    print("ip_packet_v1 = ip_packet_v2 ? {}".format(bytes(ip_packet_v1, 'utf8') == ip_packet_v2))
-    print("-------------TEST Item 5:-------------")
 
 
 if __name__ == '__main__':
